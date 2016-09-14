bcbc006c52ac78c58438e6c2aa8926f4b1f619b0
John Hetterich <hettjohn@badinhs.org>
2016-09-14 08:20:28 -0400

    first app

--- swiftApp2/ViewController.swift
+++ swiftApp2/ViewController.swift
@@ -10,9 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var coolLabel: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func button1Tapped(_ sender: AnyObject) {
+        coolLabel.text = "Now cooler!"
+        print("button tapped")
+        
+        tapCount = tapCount + 1
+        if tapCount == 10 {
+            coolLabel.text = "You've tapped 10 times"
+        }
+        
+        print(tapCount)
+
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       
+        
+        self.view.backgroundColor = UIColor.brown
+        
     }
 
     override func didReceiveMemoryWarning() {
